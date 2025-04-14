2a04071aa069cae15bf074b172212a9085454d50
dalanda2024 <sorydalendabalde@gmail.com>
2025-04-14 14:23:22 +0000

    Premier envoi du projet GandalHub

---
+++ src/front/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), 
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('course_details/', views.course_details, name='course_details'),
+    path('courses/', views.courses, name='courses'),
+    path('events/', views.events, name='events'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('starter_page/', views.starter_page, name='starter_page'),
+    path('trainers/', views.trainers, name='trainers'), 
+    
+]
